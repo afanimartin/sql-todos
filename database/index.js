d0aa041558ddea311a5081b639052bc64914641d
afanimartin <afani.dev@gmail.com>
2024-10-22 19:23:57 +0200

    Remove code to insert seed data into the database

--- database/index.js
+++ database/index.js
@@ -11,12 +11,6 @@ db.serialize(() => {
       "CREATE TABLE Todos (id INTEGER PRIMARY KEY AUTOINCREMENT, content TEXT NOT NULL, completed INTEGER DEFAULT 0 )"
     );
     console.log("Todos table created!");
-
-    db.serialize(() => {
-      db.run(
-        "INSERT INTO Todos (content) VALUES ('clean the house'), ('code todos api using sqlite3')"
-      );
-    });
   } else {
     console.log("Todos table ready to go!");
   }
