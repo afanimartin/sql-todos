d263f57560f274f962e0bfbd5bd551447860f1ad
afanimartin <afani.dev@gmail.com>
2024-10-23 06:41:51 +0200

    Format document for readability

--- src/controllers/todos.controller.js
+++ src/controllers/todos.controller.js
@@ -5,31 +5,28 @@ const add = (content) => {
   // SQLite3 does not natively return Promises
   // hence we need to wrap the database operations with a Promise
   return new Promise((resolve, reject) => {
-    db.run(
-      `INSERT INTO Todos (content) VALUES (?)`,
-      content,
-      function (error) {  // 'function' keyword is important to access 'this.lastID'
-        if (error) {
-          reject(error);
-        } else {
-          resolve({ id: this.lastID, content });  // return the inserted item's ID and content
-        }
+    db.run(`INSERT INTO Todos (content) VALUES (?)`, content, function (error) {
+      // 'function' keyword is important to access 'this.lastID'
+      if (error) {
+        reject(error);
+      } else {
+        resolve({ id: this.lastID, content }); // return the inserted item's ID and content
       }
-    );
+    });
   });
 };
 
 const fetchOne = (id) => {
   return new Promise((resolve, reject) => {
     db.get(`SELECT * FROM Todos WHERE id=?`, [id], (error, row) => {
-      if(error){
-        reject(error)
+      if (error) {
+        reject(error);
       } else {
-        resolve(row)
+        resolve(row);
       }
-    })
-  })
-}
+    });
+  });
+};
 
 // Fetch all items from the database
 const fetchAll = () => {
